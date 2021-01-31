9135fc169b2c5817e20c7af4d7f2f3d48ecd8c53
alo_wu <ilya.bela@yandex.ru>
2021-01-31 16:28:44 +0300

    ref: Properties to SphereProperties

---
+++ src/main/java/com/epam/task/third/logic/SphereProperties.java
@@ -0,0 +1,37 @@
+package com.epam.task.third.logic;
+
+import com.epam.task.third.entities.Dot;
+import com.epam.task.third.entities.Sphere;
+
+public class SphereProperties {
+
+    public boolean isSphere(Sphere sphere) {
+        double radius = sphere.getRadius();
+        return radius > 0;
+    }
+
+    public boolean isTouchAxes(Sphere sphere) {
+        Dot center = sphere.getCenter();
+        double x = center.getX();
+        double y = center.getY();
+        double z = center.getZ();
+        double r = sphere.getRadius();
+
+        boolean touchOx = touchAxe(z, r);
+        boolean touchOy = touchAxe(x, r);
+        boolean touchOz = touchAxe(y, r);
+
+        return touchOx || touchOy || touchOz;
+    }
+
+    public boolean touchAxe(double coordinate, double radius) {
+        double delta = radius - Math.abs(coordinate);
+        return delta == 0;
+    }
+
+    public boolean isCrossAxe(double coordinateOfCenter, Sphere sphere){
+        double radius = sphere.getRadius();
+        double delta = radius - Math.abs(coordinateOfCenter);
+        return delta > 0;
+    }
+}
