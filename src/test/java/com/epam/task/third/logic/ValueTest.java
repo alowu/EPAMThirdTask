1bf565149c343f1aa5b8e0ea88493117c1d080e8
alo_wu <ilya.bela@yandex.ru>
2021-01-31 16:41:51 +0300

    add: tests for volumeRatio

--- src/test/java/com/epam/task/third/logic/ValueTest.java
+++ src/test/java/com/epam/task/third/logic/ValueTest.java
@@ -7,17 +7,37 @@ import org.junit.Test;
 
 public class ValueTest {
     private final static double RADIUS = 2.8217;
-    private final static double EXPECTED = 94;
+    private final static double EXPECTED_VALUE = 94;
     private final static double DELTA = 1e3;
 
-    private final static Sphere sphere = new Sphere(new Dot(), RADIUS);
+    private final static Dot CENTER = new Dot(1, 1, 1);
+
+    private final static Sphere SPHERE = new Sphere(CENTER, RADIUS);
 
     @Test
     public void testCalculateValueShouldCalculateValueOfOrb() {
         Value value = new Value();
 
-        double result = value.calculate(sphere);
+        double result = value.calculate(SPHERE);
 
-        Assert.assertEquals(EXPECTED, result, DELTA);
+        Assert.assertEquals(EXPECTED_VALUE, result, DELTA);
+    }
+
+    @Test
+    public void testCalculateRatioShouldCalculateRatioOfVolumes() {
+        Value value = new Value();
+
+        double result = value.volumeRatio(SPHERE,'y');
+
+        Assert.assertEquals(1, result, DELTA);
+    }
+
+    @Test
+    public void testCalculateRatioShouldCalculateRatioOfVolumesWithDefaultParameter() {
+        Value value = new Value();
+
+        double result = value.volumeRatio(SPHERE);
+
+        Assert.assertEquals(3.07, result, DELTA);
     }
 }
