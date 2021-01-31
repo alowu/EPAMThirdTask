8b777b466ecd7bbb79c008b47d37ef7263c0995a
alo_wu <ilya.bela@yandex.ru>
2021-01-31 13:45:45 +0300

    add: tests for isTouchAnyAxe

--- src/test/java/com/epam/task/third/logic/PropertiesTest.java
+++ src/test/java/com/epam/task/third/logic/PropertiesTest.java
@@ -8,14 +8,20 @@ import org.junit.Test;
 public class PropertiesTest {
 
     private final double RADIUS = 5;
-    private final Sphere SPHERE = new Sphere(new Dot(), RADIUS);
     private final Sphere NOT_SPHERE = new Sphere(new Dot(), 0);
+
+    private final Dot TOUCH_AXES = new Dot(RADIUS, RADIUS, RADIUS);
+    private final Dot DONT_TOUCH = new Dot();
+
+    private final Sphere SPHERE_T = new Sphere(TOUCH_AXES, RADIUS);
+    private final Sphere SPHERE_DT = new Sphere(DONT_TOUCH, RADIUS);
+
 
     @Test
     public void testIsSphereShouldReturnTrueWhenRadiusIsPositive() {
         Properties property = new Properties();
 
-        boolean result = property.isSphere(SPHERE);
+        boolean result = property.isSphere(SPHERE_T);
 
         Assert.assertTrue(result);
     }
@@ -25,6 +31,24 @@ public class PropertiesTest {
         Properties property = new Properties();
 
         boolean result = property.isSphere(NOT_SPHERE);
+
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void testIsTouchAxes() {
+        Properties property = new Properties();
+
+        boolean result = property.isTouchAxes(SPHERE_T);
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testIsDontTouchAxes() {
+        Properties property = new Properties();
+
+        boolean result = property.isTouchAxes(SPHERE_DT);
 
         Assert.assertFalse(result);
     }
