aca6164f0bd3addd465418af6483eebb4895d153
alo_wu <ilya.bela@yandex.ru>
2021-02-05 21:40:50 +0300

    add: sphere parameters to class

---
+++ src/main/java/com/epam/task/third/parameters/SphereParameters.java
@@ -0,0 +1,42 @@
+package com.epam.task.third.parameters;
+
+import java.util.Objects;
+
+public class SphereParameters {
+    private double area;
+    private double volume;
+
+    public SphereParameters(double area, double volume) {
+        this.area = area;
+        this.volume = volume;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SphereParameters that = (SphereParameters) o;
+        return Double.compare(that.getArea(), getArea()) == 0 && Double.compare(that.getVolume(), getVolume()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getArea(), getVolume());
+    }
+
+    @Override
+    public String toString() {
+        return "SphereParameters{" +
+                "area=" + area +
+                ", volume=" + volume +
+                '}';
+    }
+}
